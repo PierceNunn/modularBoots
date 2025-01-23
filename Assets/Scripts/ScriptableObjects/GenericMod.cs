ffb15e29299ae1a268f7356b371f9bcb25dc05df
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-01-23 10:43:53 -0600

    created GenericMod.cs

---
+++ Assets/Scripts/ScriptableObjects/GenericMod.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class GenericMod : MonoBehaviour
+{
+    private string modName;
+    private string modDescription;
+
+    public string ModName { get => modName; set => modName = value; }
+    public string ModDescription { get => modDescription; set => modDescription = value; }
+}

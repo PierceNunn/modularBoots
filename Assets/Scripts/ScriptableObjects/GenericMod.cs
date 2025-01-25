31ab8f69bd9b92ba122fb562129b0ee1cc298ea0
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-01-25 13:45:28 -0600

    added field to GenericMod for icons

--- Assets/Scripts/ScriptableObjects/GenericMod.cs
+++ Assets/Scripts/ScriptableObjects/GenericMod.cs
@@ -6,7 +6,9 @@ abstract public class GenericMod : ScriptableObject
 {
     [SerializeField] private string modName;
     [SerializeField] private string modDescription;
+    [SerializeField] private Texture2D _modIcon;
 
     public string ModName { get => modName; set => modName = value; }
     public string ModDescription { get => modDescription; set => modDescription = value; }
+    public Texture2D ModIcon { get => _modIcon; set => _modIcon = value; }
 }

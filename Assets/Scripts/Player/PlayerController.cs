1b40c29cabf5e0e5375e16e63ad428a67e524719
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-01-25 13:12:15 -0600

    added WASD controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,12 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _movementSpeed;
+
+    private Vector2 movementVector;
+
+    private Rigidbody rb;
     private PlayerModsHandler modsHandler;
 
     public void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody>();
         modsHandler = gameObject.GetComponent<PlayerModsHandler>();
     }
+
+    public void FixedUpdate()
+    {
+        rb.AddForce(movementVector * _movementSpeed); //make player move
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        //set movement direction to input
+        movementVector = new Vector3(movementValue.Get<Vector2>().x, 0, movementValue.Get<Vector2>().y);
+    }
+
+
     public void OnFire()
     {
         modsHandler.FireWeapon();

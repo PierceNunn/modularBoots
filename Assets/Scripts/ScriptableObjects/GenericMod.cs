6c70cbc647af15510fd682c958b915de091ad576
PierceNunn <agentp5151@gmail.com>
2025-01-27 14:10:10 -0600

    added "ammo cost" to mods

--- Assets/Scripts/ScriptableObjects/GenericMod.cs
+++ Assets/Scripts/ScriptableObjects/GenericMod.cs
@@ -8,6 +8,8 @@ abstract public class GenericMod : ScriptableObject
     [SerializeField] private string modDescription;
     [SerializeField] private Sprite _modIcon;
 
+    [SerializeField] private float _ammoCost;
+
     public string ModName { get => modName; set => modName = value; }
     public string ModDescription { get => modDescription; set => modDescription = value; }
     public Sprite ModIcon { get => _modIcon; set => _modIcon = value; }

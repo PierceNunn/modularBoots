56628245a89a487855fec17bd1f161240775e394
PierceNunn <agentp5151@gmail.com>
2025-01-27 14:11:12 -0600

    added cooldown variable to mods

--- Assets/Scripts/ScriptableObjects/GenericMod.cs
+++ Assets/Scripts/ScriptableObjects/GenericMod.cs
@@ -9,8 +9,11 @@ abstract public class GenericMod : ScriptableObject
     [SerializeField] private Sprite _modIcon;
 
     [SerializeField] private float _ammoCost;
+    [SerializeField] private float _cooldown;
 
     public string ModName { get => modName; set => modName = value; }
     public string ModDescription { get => modDescription; set => modDescription = value; }
     public Sprite ModIcon { get => _modIcon; set => _modIcon = value; }
+    public float AmmoCost { get => _ammoCost; set => _ammoCost = value; }
+    public float Cooldown { get => _cooldown; set => _cooldown = value; }
 }

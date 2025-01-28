0ac0779068769dc4920eeb6eac7a896975b4e6b2
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-01-28 14:48:49 -0600

    debug ui displays ammo count

--- Assets/Scripts/DebugManager.cs
+++ Assets/Scripts/DebugManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DebugManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI _debugText;
+
+    private PlayerResources pr;
+
     void Start()
     {
-        
+        pr = FindObjectOfType<PlayerResources>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _debugText.text = "Current Ammo: " + pr.CurrentAmmo + "/" + pr.MaxAmmo;
     }
 }

461c1cf816a9179a61431d05099cb645fce091ae
PierceNunn <agentp5151@gmail.com>
2025-02-17 14:29:47 -0600

    made current and max health gettable and settable outside HealthSystem

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -9,12 +9,15 @@ public class HealthSystem : MonoBehaviour
     
     private float currentHealth;
     private CanDie entityController;
-    
+
+    public float MaxHealth { get => maxHealth; set => maxHealth = value; }
+    public float CurrentHealth { get => currentHealth; set => currentHealth = value; }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
+        CurrentHealth = MaxHealth;
         entityController = GetComponent<CanDie>();
     }
 
@@ -27,8 +30,8 @@ public class HealthSystem : MonoBehaviour
     public void TakeDamage(float damage)
     {
         Debug.Log(name + " took " + damage + " damage.");
-        currentHealth -= damage;
-        if (currentHealth <= 0) 
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0) 
         {
             AudioManager.Instance.PlaySFX("Enemy Death");
             entityController.Die();
@@ -37,10 +40,10 @@ public class HealthSystem : MonoBehaviour
 
     public void ReceiveHealing(float healing)
     {
-        currentHealth += healing;
-        if (currentHealth > maxHealth)
+        CurrentHealth += healing;
+        if (CurrentHealth > MaxHealth)
         {
-            currentHealth = maxHealth;
+            CurrentHealth = MaxHealth;
         }
     }
 

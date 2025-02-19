5c3aad6fa791a67f5f41776b8216784da4b24e25
samwellHamwell <shooker@mail.bradley.edu>
2025-02-19 14:53:31 -0600

    Pause menu started

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseScreen;
+    [SerializeField] private GameObject ControlsScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseScreen.SetActive(false);
+        //ControlsScreen.SetActive(false);
+    }
+
+    public void OnPause()
+    {
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void onResume()
+    {
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void onExit()
+    {
+        Application.Quit();
+    }
+
+    public void onControls()
+    {
+        ControlsScreen.SetActive(true);
+    }
+    public void onBack()
+    {
+        ControlsScreen.SetActive(false);
+    }
+
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
